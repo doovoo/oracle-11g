ca0b0d523ba97fba158e43e52d2835309f335ddf
Minchul Kim <doovooda@pinnode.com>
2020-03-02 16:55:55 +0900

    [configue]
    1. 컨테이너 실행 시 계정 생성

--- assets/setup.sh
+++ assets/setup.sh
@@ -14,24 +14,6 @@ deps () {
 
 }
 
-users () {
-
-	echo "Configuring users"
-	groupadd -g 200 oinstall
-	groupadd -g 201 dba
-	useradd -u 440 -g oinstall -G dba -d /opt/oracle oracle
-	echo "oracle:install" | chpasswd
-	echo "root:install" | chpasswd
-	sed -i "s/pam_namespace.so/pam_namespace.so\nsession    required     pam_limits.so/g" /etc/pam.d/login
-	mkdir -p -m 755 /opt/oracle/app
-	mkdir -p -m 755 /opt/oracle/oraInventory
-	mkdir -p -m 755 /opt/oracle/dpdump
-	chown -R oracle:oinstall /opt/oracle
-	cat /assets/profile >> ~oracle/.bash_profile
-	cat /assets/profile >> ~oracle/.bashrc
-
-}
-
 sysctl_and_limits () {
 
 	cp /assets/sysctl.conf /etc/sysctl.conf
@@ -40,5 +22,4 @@ sysctl_and_limits () {
 }
 
 deps
-users
 sysctl_and_limits
